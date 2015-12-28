df417207cc934cc85a47ed63d24915a3607313ec
Kirill <ravijkee@yandex.ru>
2015-12-29 01:34:30 +0300

    Update

--- Queue/Queue.h
+++ Queue/Queue.h
@@ -1,12 +1,8 @@
-﻿#pragma once
+﻿#pragma once 
 #include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
-#include <tchar.h>
-#include <locale.h>
 #include <ctime>
-using namespace std;
 
+using namespace std;
 template <class T>
 class TQueue
 {
@@ -14,7 +10,70 @@ private:
 	int begin, end, len, MaxSize;
 	T *Array;
 public:
-	friend ostream &operator<<(ostream &out, const TQueue<T> &Q)
+	TQueue(int _MaxSize = 10)
+	{
+		MaxSize = _MaxSize;
+		Array = new T[MaxSize];
+		begin = 0;
+		end = -1;
+		len = 0;
+	}
+	~TQueue()
+	{
+		delete[]Array;
+	}
+
+	TQueue(const TQueue &Q)
+	{
+		MaxSize = Q.MaxSize;
+		begin = Q.begin;
+		len = Q.len;
+		end = Q.end;
+	}
+
+	bool IsEmpty()
+	{
+		return (len == 0);
+	}
+
+	bool IsFull()
+	{
+		return (len == MaxSize);
+	}
+
+	void Push(const T n)
+	{
+		if (end < MaxSize - 1)
+			end++;
+		else end = 0;
+		Array[end] = n;
+		len++;
+	}
+
+	T Pop()
+	{
+		T tmp = Array[begin];
+		if (IsEmpty())
+			throw "Очередь пуста";
+		else
+		{
+			if (begin < MaxSize - 1)
+				begin++;
+			else
+				begin = 0;
+		}
+		len--;
+		return tmp;
+	}
+
+	T Top()
+	{
+		if (IsEmpty())
+			throw - 1;
+		return Array[begin];
+	}
+
+	friend ostream& operator<<(ostream &out, const TQueue <T> &Q)
 	{
 		if (Q.len == 0)
 			out << "Очередь пуста\n";
@@ -28,84 +87,16 @@ public:
 		return out;
 	}
 
-	TQueue(int _maxsize = 10)
+	int GetLen()
 	{
-		if (_maxsize <= 0)
-			throw _maxsize;
-		MaxSize = _maxsize;
-		Array = new T[MaxSize];
-		begin = 0;
-		end = -1;
-		len = 0;
+		return len;
 	}
 
-	virtual ~TQueue() 
-	{ 
-		delete[] Array; 
+	int GetMaxSize()
+	{
+		return MaxSize;
 	}
 
-	TQueue(const TQueue& q)
-	{
-		MaxSize = q.MaxSize;
-		begin = q.begin;
-		end = q.end;
-		len = q.len;
-		Array = new T[MaxSize];
-		for (int i = 0; i < MaxSize; i++)
-			Array[i] = q.Array[i];
-	}
-
-	bool IsEmpty()
-	{
-		return len == 0;
-	}
-
-	bool IsFull()
-	{
-		return len == MaxSize;
-	}
-
-	void Push(const T n)
-	{
-		if (len == MaxSize)
-			throw len;
-		if (end < MaxSize - 1)
-			end++;
-		else end = 0;
-		len++;
-		Array[end] = n;
-	}
-
-	T Pop()
-	{
-		if (!len)
-			throw - 1;
-		T tmp = Array[begin];
-		if (begin < MaxSize - 1)
-			begin++;
-		else begin = 0;
-		len--;
-		return tmp;
-	}
-
-	T Front()
-	{
-		if (!len)
-			throw - 1;
-		return Array[begin];
-	}
-
-	T Back()
-	{
-		if (!len)
-			throw - 1;
-		return Array[end];
-	}
-
-	void Clear()
-	{
-		begin = 0;
-		end = -1;
-		len = 0;
-	}
 };
+
+
